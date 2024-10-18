94e51194e9296a25136c0134b7cf29492f78cc8f
OneAndOnlyBarrettt <92542848+OneAndOnlyBarrettt@users.noreply.github.com>
2024-10-18 15:04:58 -0400

    initial commit

---
+++ baritone.js
@@ -0,0 +1,8 @@
+const playerX = null;
+const playerY = null;
+const destinationX = prompt("X DESTINATION");
+const destinationY = prompt("Y DESTINATION");
+
+function movePlayer() {
+    requestAnimationFrame(movePlayer());
+};
