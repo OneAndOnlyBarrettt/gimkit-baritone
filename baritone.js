6babd03f77b73d4f34454b4aa8f7b5c8e704ccd0
OneAndOnlyBarrettt <92542848+OneAndOnlyBarrettt@users.noreply.github.com>
2024-10-18 22:54:52 -0700

    add window.stores functionality, + add onto movePlayer function

--- baritone.js
+++ baritone.js
@@ -1,8 +1,25 @@
-const playerX = null;
-const playerY = null;
+const playerX = window.stores.phaser.mainCharacter.body.x;
+const playerY = window.stores.phaser.mainCharacter.body.y;
 const destinationX = prompt("X DESTINATION");
 const destinationY = prompt("Y DESTINATION");
 
 function movePlayer() {
+    if (playerX < destinationX) {
+
+    };
+
+    if (playerX > destinationX) {
+
+    };
+
+    if (playerY < destinationY) {
+
+    };
+
+    if (playerY > destinationY) {
+
+    };
     requestAnimationFrame(movePlayer());
 };
+
+movePlayer();
